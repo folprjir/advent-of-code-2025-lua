dc8202eb4706395fc7a228df456c3452ea76c9cf
Jiří Folprecht <folprjir@gmail.com>
2026-07-17 18:10:44 +0200

    remove debug commented debug prints from day 15 task 1

--- tasks/day_15/day15_task1.lua
+++ tasks/day_15/day15_task1.lua
@@ -17,7 +17,6 @@ while true do
 	if line == "" then
 		break
 	end
-
 	row_i = row_i + 1
 	g[row_i] = {}
 	for col = 1, #line do
@@ -27,8 +26,6 @@ end
 
 local max_row = row_i
 local max_col = #g[1]
-
--- print(string.format("rows: %d, colums: %d", max_row, max_col))
 
 local function print_g()
 	local str = ""
@@ -60,7 +57,6 @@ local str = ""
 for i = 1, #steps do
 	str = str .. steps[i]
 end
-
 print("\n steps:", str)
 
 local robot_row, robot_col = 0, 0
@@ -77,44 +73,34 @@ end
 local function move(dir, row, col)
 	local new_row, new_col = row, col
 	if dir == ">" then
-		-- print("moovin right")
 		new_col = col + 1
 	end
 	if dir == "<" then
-		--print("moovin left")
 		new_col = col - 1
 	end
 	if dir == "v" then
-		-- print("moovin down")
 		new_row = row + 1
 	end
 	if dir == "^" then
-		-- print("moovin up")
 		new_row = row - 1
 	end
-	-- print("movin to:", new_row, new_col)
 	return new_row, new_col
 end
 
 local function move_back(dir, row, col)
 	local new_row, new_col = row, col
 	if dir == ">" then
-		-- print("swapin right")
 		new_col = col - 1
 	end
 	if dir == "<" then
-		-- print("swapin left")
 		new_col = col + 1
 	end
 	if dir == "v" then
-		-- print("swapin down")
 		new_row = row - 1
 	end
 	if dir == "^" then
-		-- print("swapin up")
 		new_row = row + 1
 	end
-	-- print("swapin to:", new_row, new_col)
 	return new_row, new_col
 end
 
@@ -130,7 +116,6 @@ local function move_robot(dir, row_, col_)
 	local new_robot_row, new_robot_col = row_, col_
 
 	local function inner(row, col)
-		-- print(string.format("dir %s, row: %d, col: %d", dir, row, col))
 		local c = g[row][col]
 		if c == "#" then
 			success = false
@@ -143,22 +128,18 @@ local function move_robot(dir, row_, col_)
 		row, col = move(dir, row, col)
 		inner(row, col)
 		if success then
-			-- print("swapin")
 			swap(dir, row, col)
 			new_robot_row, new_robot_col = row, col
 		else
-			-- print("no swapin :(")
 		end
 	end
 
 	inner(row_, col_)
-	-- print("new_row", new_robot_row, "new_col", new_robot_col)
 	return new_robot_row, new_robot_col
 end
 
 for i = 1, #steps do
 	robot_row, robot_col = move_robot(steps[i], robot_row, robot_col)
-	-- print_g()
 end
 
 local sum = 0
@@ -169,17 +150,5 @@ for row = 1, max_row do
 		end
 	end
 end
-print(sum)
 
---[[
-robot_row, robot_col = move_robot("^", robot_row, robot_col)
-print_g()
-robot_row, robot_col = move_robot("^", robot_row, robot_col)
-print_g()
-robot_row, robot_col = move_robot(">", robot_row, robot_col)
-print_g()
-robot_row, robot_col = move_robot(">", robot_row, robot_col)
-print_g()
-robot_row, robot_col = move_robot("v", robot_row, robot_col)
-print_g()
-]]
+print(string.format("The result is: %d", sum))
